d8cab5089fce1d707b4ad14ed6722d645fe17e42
mrgangwar <balram72500@gmail.com.com>
2026-01-19 13:17:56 +0530

    first commit

---
+++ src/app/register/page.tsx
@@ -0,0 +1,156 @@
+'use client'
+import React, { useState } from 'react'
+import { AnimatePresence, motion } from "motion/react"
+import { TbPlayerTrackNext } from 'react-icons/tb';
+import { FaEye , FaEyeSlash} from "react-icons/fa";
+import { FcGoogle }  from 'react-icons/fc';
+import { useRouter } from 'next/navigation';
+import axios from 'axios';
+import { ClipLoader } from 'react-spinners';
+import { signIn } from 'next-auth/react';
+
+
+const register = () => {
+    const [step , steStep] = useState<1 | 2 >(1)
+    const [name , setName] =useState("")
+    const [email , setEmail] =useState("")
+    const [password , setPassword] =useState("")
+    const [showPassword , setShowPassword] =useState(false)
+    const router = useRouter()
+    const  [loading , setLoading] = useState(false)
+
+    const handlesignUp = async (e:React.FormEvent) =>{
+        e.preventDefault()
+        setLoading(true)
+        try{
+            const result = await axios.post('/api/auth/register',{name , email, password})
+            console.log(result.data)
+            setLoading(false)
+            setEmail("")
+            setName("")
+            setPassword("")
+            router.push('/login')
+        } catch (error){
+            console.log("Registration Error: ", error)
+            setLoading(false)
+        }
+    }
+
+
+  return (
+    <div className='min-h-screen flex items-center justify-center bg-gradient-to-br from-gray-900 via-black to-gray-900 text-white p-6'>
+        <AnimatePresence mode='wait'>
+        {/* step 1 UI */}
+        {step == 1 && 
+        <motion.div 
+        initial = {{opacity:0 , y:40}}
+        animate = {{opacity:1 , y:0}}
+        exit = {{opacity:0, y:-40}}
+        transition={{duration:0.5}}
+        className='w-full max-w-lg text-center bg-white/10 backdrop-blur-md rounded-2xl shadow-28xl p-10 border-white/20'>
+            <h1 className='text-4xl font-bold mb-4 text-blue-400'>Welcome to Shopsyra</h1>
+            <p className='text-grey-300 mb-6'> Register with one of the following account type</p>
+            <div className='grid grid-col-3 gap-4 mb-6'>
+                {
+                    [
+                    {label: "User", icon: "👤", value: "user"},
+                    {label: "Seller", icon: "🏪", value: "seller"},
+                    {label: "Admin", icon: "🛠️", value: "admin"},
+                    ].map((item)=>(
+                        <motion.div 
+                        key={item.value}
+                        whileHover={{ scale:1.1}}
+                        whileTap={{ scale:0.95}}
+                        className='p-4 bg-white/5 hover:bg-white/20 cursor-pointer rounded-xl border border-white/30 shadow-lg flex flex-col items-center transition'
+                        >
+                            <span className='text-4xl mb-2'>{item.icon}</span>
+                            <span className='text-sm font-medium'>{item.value}</span>
+
+                        </motion.div>
+                    ))
+                }
+            </div>
+
+
+            <motion.button
+            onClick={()=>steStep(2)}
+            whileHover={{ scale:1.03}}
+             whileTap={{ scale:0.95}}
+             className='mt-4 px-8 py-3 flex item-center justify-center gap-2 bg-blue-500 hover:bg-blue-600 rounded-xl font-medium w-full '
+            >Next<TbPlayerTrackNext className='mt-1.5'/></motion.button>
+
+        </motion.div>}
+        {/* step 2 UI */}
+         {step == 2 &&
+        <motion.div
+         initial = {{opacity:0 , y:40}}
+        animate = {{opacity:1 , y:0}}
+        exit = {{opacity:0, y:-40}}
+        transition={{duration:0.5}}
+        className='w-full max-w-md bg-white/10 backdrop-blur-md rounded-2xl shadow-2xl p-8 border-white/20'
+        >
+            <h1 className='text-2xl font-semibold text-center mb-6 text-blue-300'>Create your Account</h1>
+
+            <form onSubmit={handlesignUp} className='flex flex-col gap-4'>
+                <input
+                type='text'
+                required
+                placeholder='Full Name'
+                className='bg-white/10 border border-white/30 rounded-lg p-3 focus:outline-none focus:ring-2 focus:ring-blue-500'
+                onChange={(e)=>setName(e.target.value)} value={name}
+                />
+                <input
+                type='text'
+                required
+                placeholder='Email Address'
+                className='bg-white/10 border border-white/30 rounded-lg p-3 focus:outline-none focus:ring-2 focus:ring-blue-500'
+                onChange={(e)=>setEmail(e.target.value)} value={email}
+                />
+                
+                <input
+                type= {showPassword ? "text" : "password"}
+                required
+                placeholder='Password'
+                className='bg-white/10 relative border border-white/30 rounded-lg p-3 focus:outline-none focus:ring-2 focus:ring-blue-500'
+                onChange={(e)=>setPassword(e.target.value)} value={password}
+                />
+                <button 
+                type='button'
+                onClick={()=>setShowPassword(!showPassword)}
+                className='absolute right-12 top-61 -translate-y-1/2 text-grat-400 hover:text-white transition'>
+                    {showPassword ? <FaEyeSlash size={18}/> : <FaEye size={18}/>}
+                </button>
+
+                <motion.button
+                disabled={loading}
+           type='submit'
+            whileHover={{ scale:1.03}}
+             whileTap={{ scale:0.95}}
+             className='mt-4 px-8 py-3 flex item-center justify-center gap-2 bg-blue-500 hover:bg-blue-600 rounded-xl font-medium w-full '>
+           {loading ? <ClipLoader size={20} color="white" /> : "Register Now"}</motion.button>
+
+
+            <div className='flex items-center my-3'>
+                <div className='flex-1 h-px bg-gray-600'></div>
+                <span className='px-3 text-sm text-gray-400'>Or</span>
+                <div className='flex-1 h-px bg-gray-600'></div>
+            </div>
+
+             <motion.button
+             onClick={()=>signIn("google", {callbackUrl:"/"})}
+            whileHover={{ scale:1.03}}
+             whileTap={{ scale:0.95}}
+             className='flex items-center justify-center gap-3 py-3 bg-white/10 hover:bg-white/20 border-white/30 rounded-xl transition'
+            ><FcGoogle className='w-5 h-5'/><span className='font-meduim'>Continue With Google</span></motion.button>
+            <p className='text-center text-smmt-4 text-gray-400'>Already have an account{" "}</p>
+             <span onClick={()=>router.push("/login")} className='flex item-center justify-center text-blue-400 hover:underline hover:text-blue-300 transition'>SignIn</span>
+                
+            </form>
+
+        </motion.div>}
+        </AnimatePresence>
+    </div>
+  )
+}
+
+export default register
---
+++ src/global.ts
@@ -0,0 +1,12 @@
+import mongoose, { Connection } from "mongoose";
+
+
+declare global{
+    var mongoose:{
+        conn:Connection | null,
+        promise:Promise<Connection> | null
+    }
+}
+
+
+export {}
